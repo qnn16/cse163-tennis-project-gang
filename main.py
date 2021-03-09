3ce4dc17582145ffd80585d03e1cb52f933b10c1
Quang Nguyen <qnn@uw.edu>
2021-03-08 17:53:04 -0800

    court surface stuff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px
 import glob
 
 
@@ -13,8 +14,53 @@ def combine_file(directory):
 
 
 def main():
-    directory = r'C:\Users\steve\OneDrive\Documents\GitHub\cse163-tennis-project-gang\data'
-    print(combine_file(directory))
+    directory = 'data/'
+    df = combine_file(directory)
+    court_surface(df)
+
+def court_surface(df):
+    winner = df.groupby(['winner_name', 'surface'], as_index=False).size()
+
+    loser = df.groupby(['loser_name', 'surface'], as_index=False).size()
+
+    winner_loser = winner.merge(loser, left_on=['winner_name', 'surface'], right_on=['loser_name', 'surface'], how='inner')
+    winner_loser['surface_total'] = winner_loser['size_x'] + winner_loser['size_y']
+    winner_loser['total_matches'] = winner_loser['surface_total'].groupby(winner_loser['winner_name']).transform('sum')
+    winner_loser = winner_loser.rename(columns={'winner_name': 'name', 'size_x': 'won', 'size_y': 'lost'})
+    winner_loser = winner_loser.drop(['loser_name'], axis=1)
+    winner_loser['win_rate'] = round((winner_loser['won'] / winner_loser['surface_total']) * 100, 2)
+    winner_loser.to_csv('winner_loser.csv')
+
+    grass_court = winner_loser[winner_loser['surface'] == 'Grass']
+    grass_court_top10 = grass_court.nlargest(10, 'won')
+    fig1 = px.bar(grass_court_top10, x='name', y='won', color_discrete_sequence =['green'], hover_data=['surface_total', 'win_rate'],
+                 labels={
+                     'name': 'Player Name',
+                     'won': 'Games Won',
+                     'surface_total': 'Total Matches on Grass',
+                     'win_rate': 'Winning Percentage on Grass'
+                 })
+    fig1.update_layout(title_text='Players With the Most Wins on Grass Court', title_x=0.5)
+    fig1.show()
+
+    clay_court = winner_loser[winner_loser['surface'] == 'Clay'] #fig 3
+    clay_court_top10 = clay_court.nlargest(10, 'won')
+    fig3 = px.bar(clay_court_top10, x='name', y='won', color_discrete_sequence =['red'], hover_data=['surface_total', 'win_rate'],
+                 labels={
+                     'name': 'Player Name',
+                     'won': 'Games Won',
+                     'surface_total': 'Total Matches on Clay',
+                     'win_rate': 'Winning Percentage on Clay'
+                 })
+    fig3.update_layout(title_text='Players With the Most Wins on Clay Court', title_x=0.5)
+    fig3.show()
+
+
+    # hard_court = winner_loser[winner_loser['surface'] == 'Hard'] #fig 2
+    # print(hard_court.nlargest(10, 'won'))
+
+    # carpet_court = winner_loser[winner_loser['surface'] == 'Carpet'] #fig 4
+    # print(carpet_court.nlargest(10, 'won'))
 
 
 if __name__ == '__main__':
